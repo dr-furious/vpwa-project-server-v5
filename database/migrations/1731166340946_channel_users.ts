9062eed305d56db2c87d9ebede4295b8926f5908
Martin Sivak <xsivakm1@stuba.sk>
2024-11-09 16:42:25 +0100

    feat: create models, define relationships

--- database/migrations/1731166340946_channel_users.ts
+++ database/migrations/1731166340946_channel_users.ts
@@ -1,24 +1,26 @@
 import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = "messages";
+  protected tableName = "channel_users";
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
       table
-        .integer("created_by")
+        .integer("user_id")
         .unsigned()
+        .notNullable()
         .references("id")
         .inTable("users")
         .onDelete("CASCADE");
       table
         .integer("channel_id")
         .unsigned()
+        .notNullable()
         .references("id")
         .inTable("channels")
         .onDelete("CASCADE");
-      table.text("content");
+      table.unique(["user_id", "channel_id"]);
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
